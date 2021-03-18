43e7b152858fd68f456d07c4bbb6b41336582133
root <root@ubuntu.ubuntu-domain>
2021-03-18 09:12:14 +0100

    first commit

---
+++ public/map.js
@@ -0,0 +1,32 @@
+var map = new ol.Map({
+    target: 'map',
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM(),
+        opacity : 1
+      }),
+      new ol.layer.Vector({
+        source: new ol.source.Vector({
+          url: 'geo-search-results-json' + window.location.search,
+          format: new ol.format.GeoJSON()
+        }),
+        style: new ol.style.Style({
+          image: new ol.style.Circle({
+            radius: 10,
+            stroke: new ol.style.Stroke({
+              color: 'black',
+              width: 4
+            }),
+            fill: new ol.style.Fill({
+              color: 'yellow'
+            })
+          })
+        }),
+      })
+    ],
+    view: new ol.View({
+      center: ol.proj.fromLonLat([2.584, 48.841]),
+      zoom: 12,
+      maxZoom: 20
+    })
+  });
